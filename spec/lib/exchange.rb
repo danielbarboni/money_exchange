8a5041dcfd70298d618d7c88c29c84b9c0c9062c
Daniel Barboni <danielbarboni@hotmail.com>
2018-01-10 01:40:26 -0200

    Lib Exchange File

---
+++ spec/lib/exchange.rb
@@ -0,0 +1,15 @@
+require 'rest-client'
+require 'json'
+
+module Currency
+  def self.exchange currency, currency_destination, quantity
+    begin
+      res = RestClient.get "http://api.fixer.io/latest?base=#{currency}&symbols=#{currency_destination}"
+      value = JSON.parse(res.body)['rates'][currency_destination]
+      puts "Quantity: #{quantity}, Currency: #{currency}, Value: #{(value * quantity).round(2)},  Currency_Destination: #{currency_destination}"
+      return (value * quantity).round(2)
+    rescue
+      return 'Problema na Conversão'
+    end
+  end
+end
